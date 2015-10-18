8db776029af714ce2abe72836a421e184682acec
Mark Jacob Pritt <mpritt4@login.(none)>
2015-10-18 19:52:10 -0400

    Updated accuracy scripts

--- scripts/flux/split_replicates.py
+++ scripts/flux/split_replicates.py
@@ -7,58 +7,51 @@ import random
 Split the given fastq file into 2 files
 """
 
-def split_replicates(reads1, reads2, output_prefix):
-    lines1 = []
-    lines2 = []
+def split_replicates(input, dir_prefix, num, split_random, prob):
+    with open(input, 'r') as f:
+        output = []
+        for i in range(num):
+            output.append([open(dir_prefix+str(i)+'/reads1.fastq', 'w'), open(dir_prefix+str(i)+'/reads2.fastq', 'w')])
 
-    descriptor = reads1.readline().rstrip()
-    while descriptor:
-        lines1 += [descriptor + '\n' + reads1.readline() + reads1.readline() + reads1.readline()]
-        lines2 += [reads2.readline() + reads2.readline() + reads2.readline() + reads2.readline()]
+        i = 0
+        descriptor1 = f.readline()
+        while descriptor1:
+            for _ in range(3):
+                descriptor1 += f.readline()
+            descriptor2 = ''
+            for _ in range(4):
+                descriptor2 += f.readline()
 
-        descriptor = reads1.readline().rstrip()
+            if split_random:
+                for id in range(num):
+                    if random.random() < prob:
+                        output[id][0].write(descriptor1)
+                        output[id][1].write(descriptor2)
+            else:
+                output[i][0].write(descriptor1)
+                output[i][1].write(descriptor2)
+                i = (i+1) % num
 
+            descriptor1 = f.readline()
 
-    len1 = len(lines1) / 2
-    order = [0] * len1 + [1] * (len(lines1)-len1)
-    random.shuffle(order)
-
-    f1_1 = open(output_prefix + '1_1.fastq', 'w')
-    f1_2 = open(output_prefix + '1_2.fastq', 'w')
-    f2_1 = open(output_prefix + '2_1.fastq', 'w')
-    f2_2 = open(output_prefix + '2_2.fastq', 'w')
-
-    for i in xrange(len(order)):
-        if (order[i] == 0):
-            f1_1.write(lines1[i])
-            f1_2.write(lines2[i])
-        else:
-            f2_1.write(lines1[i])
-            f2_2.write(lines2[i])
-
-
-    f1_1.close()
-    f1_2.close()
-    f2_1.close()
-    f2_2.close()
-
+        for i in range(num):
+            output[i][0].close()
+            output[i][1].close()
 
 if __name__ == '__main__':
     # Print file's docstring if -h is invoked
     parser = argparse.ArgumentParser(description=__doc__, 
             formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('--input-prefix', type=str, required=True, 
+    parser.add_argument('--input-fastq', type=str, required=True, 
         help='Full path of fastq file containing paired end reads')
-    parser.add_argument('--output-prefix', type=str, required=False, 
+    parser.add_argument('--output-dir', type=str, required=True, 
         help='Full path to location and prefix to write output file')
+    parser.add_argument('--n', type=int, required=True, help='Number of files to split into')
+    parser.add_argument('--random', action='store_true', help='Assign each read to each file with probability specified by --prob')
+    parser.add_argument('--prob', type=float, required=False, help='If random flag is set, assign each read to each file with this probability')
 
     args = parser.parse_args(sys.argv[1:])
 
-    if args.output_prefix:
-        output_prefix = args.output_prefix
-    else:
-        output_prefix = args.input_name.split('.')[0]
-
-    with open(args.input_prefix + '_1.fastq') as reads1:
-        with open(args.input_prefix + '_2.fastq') as reads2:
-            split_replicates(reads1, reads2, output_prefix)
+    if args.random and not args.prob:
+        args.prob = 1.0 / float(args.n)
+    split_replicates(args.input_fastq, args.output_dir, args.n, args.random, args.prob)
