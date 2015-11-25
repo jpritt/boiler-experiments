a74e12114dfaf55a476b2cc9c9d0f80821e04d27
Mark Jacob Pritt <mpritt4@login.(none)>
2015-11-25 14:15:45 -0500

    Bug fix to SAM comparison

---
+++ scripts/tech_reps/paired/eval.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sizes=('5' '10' '25' '50' '100')
+ns=('0')
+#ns=('0' '1' '2' '3' '4')
+
+for i in "${sizes[@]}";
+do
+    for j in "${ns[@]}";
+    do
+        qsub -vDIR=/scratch0/langmead-fs1/user/jacob/compress-alignments-test/drosophila/tech_reps/paired$i/sim$j ../eval_no_ref.pbs
+        sleep 1
+        qsub -vDIR=/scratch0/langmead-fs1/user/jacob/compress-alignments-test/drosophila/tech_reps/paired$i/sim$j ../eval_with_ref.pbs
+        sleep 1
+    done
+done
