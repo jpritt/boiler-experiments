0eb9590615b4f4f59ae6f608876a0946e8cd1779
Mark Jacob Pritt <mpritt4@login.(none)>
2015-12-15 17:56:07 -0500

    Fixed off-by-one error in read locations

--- scripts/remove_read_names.py
+++ scripts/remove_read_names.py
@@ -9,20 +9,12 @@ Usage: ./remove_read_names.py input.sam output.sam
 
 with open(sys.argv[1], 'r') as f_in:
     with open(sys.argv[2], 'w') as f_out:
-        read_names = []
-
+        i = 0
         for line in f_in:
             row = line.rstrip().split('\t')
             if len(row) < 6:
                 f_out.write(line)
             else:
-                name = row[0]
-                found = False
-                for i in range(len(read_names)):
-                    if read_names[i] == name:
-                        row[0] = str(i)
-                        found = True
-                if not found:
-                    row[0] = str(len(read_names))
-                    read_names.append(name)
+                row[0] = str(i)
                 f_out.write('\t'.join(row) + '\n')
+                #i += 1
